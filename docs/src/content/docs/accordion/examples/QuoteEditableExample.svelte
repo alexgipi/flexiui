e5be6675b991e8709deefea69ef6d12eb136c3dd
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:42:49 +0200

    feat: add QuoteBasicExample and QuoteEditableExample components for demonstrating quote functionality

---
+++ docs/src/content/docs/accordion/examples/QuoteEditableExample.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Quote } from "@flexiui/svelte-quote";
+  let content = `This is a quote component example with editable mode enabled.`;
+
+  function handleFocus(e: any) {
+    console.log("Focus event");
+    console.log(e);
+  }
+
+  function handleKeyDown(e: any) {
+    console.log("Keydown event");
+    console.log(e);
+  }
+
+  function handleInput(e: any) {
+    console.log("Input event");
+    console.log(e);
+  }
+</script>
+
+<Quote
+  className="my-quote"
+  id="my-quote-id"
+  bgColor="transparent"
+  editable={true}
+  bind:value={content}
+  on:focus={handleFocus}
+  on:keydown={handleKeyDown}
+  on:input={handleInput}
+></Quote>
