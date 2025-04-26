149ce1f98edc78df05837875dce5ec9ed888d60e
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:48:28 +0200

    feat: add TagsInput component example and documentation

---
+++ docs/src/content/docs/tags-input/examples/TagsInputBasicExample.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { TagsInput } from "@flexiui/svelte-tags-input";
+
+    let labels = {
+        plural: "Tags",
+        singular: "Tag",
+    };
+
+    let tags = ["Banana", "Apple", "Mango", "Grape", "Orange", 
+    "Pineapple", "Strawberry", "Blueberry", "Watermelon"];
+
+</script>
+
+<TagsInput
+  name="tags-input-basic-example"
+  {labels}
+  {tags}
+  min={1}
+  max={20}
+  showCounter={true}
+  allowDuplicates={false}
+  bidimensional={false}
+  bidimensionalSeparator="->"
+  placeholder={"Press enter to add a new tag"}
+  required={false}
+/>
