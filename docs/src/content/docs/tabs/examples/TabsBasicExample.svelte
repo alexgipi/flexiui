dd1fab1ffa79f665373d0a832a15b01c5fa48811
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:48:03 +0200

    feat: add Tabs component example and documentation with beta warning

---
+++ docs/src/content/docs/tabs/examples/TabsBasicExample.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Tabs from '@components/ui/Tabs/Tabs.svelte';
+  import TabPanel from '@components/ui/Tabs/TabPanel.svelte';
+
+  let activeTabIndex = 0;
+  let tabs = [
+    {
+      name: "exercise_content",
+      label: "Contenido del ejercicio",
+    },
+    {
+      name: "exercise_settings",
+      label: "Ajustes del ejercicio",
+    },
+  ];
+</script>
+
+<Tabs {tabs} bind:activeTabIndex>
+  <TabPanel active={activeTabIndex === 0}>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis quod incidunt ullam necessitatibus rerum sunt fugiat et consequatur in at similique molestias nesciunt dolores, impedit modi aliquid! Fuga, vero?
+  </TabPanel>
+  <TabPanel active={activeTabIndex === 1}>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit illo quisquam aspernatur voluptatem laborum itaque dolores nemo culpa aliquid. Ipsum at dolorum a nulla nemo fugiat enim repudiandae ut quae!
+  </TabPanel>
+</Tabs>
