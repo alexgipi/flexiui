79e623d76a91b1b78ae4e375c9d2b58703471319
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:41:41 +0200

    feat: update Textarea component styles for improved aesthetics and readability

--- docs/src/components/ui/Textarea.svelte
+++ docs/src/components/ui/Textarea.svelte
@@ -8,12 +8,13 @@
 
 <style>
     textarea {
-        border: 1px solid #27272a;
+        border: 1px solid #222222a3;
         padding-inline: .75rem;
         padding-block: .5rem;
         min-height: 80px;
         resize: vertical;
         font-family: system-ui;
+        font-size: .875rem;
         border-radius: .5rem;
         background: transparent;
     }
