71d778275a1557585910fe99ab2ad85b5d0eae7f
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:42:38 +0200

    feat: add BasicExample component to demonstrate Accordion usage with accessibility and styling information

---
+++ docs/src/content/docs/accordion/examples/BasicExample.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Accordion from "@components/ui/Accordion/Accordion.svelte";
+  import AccordionItem from "@components/ui/Accordion/AccordionItem.svelte";
+  import AccordionSummary from "@components/ui/Accordion/AccordionSummary.svelte";
+  import AccordionContent from "@components/ui/Accordion/AccordionContent.svelte";
+</script>
+
+<Accordion>
+  <AccordionItem>
+    <AccordionSummary slot="summary">Is it accessible?</AccordionSummary>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+
+  <AccordionItem>
+    <AccordionSummary slot="summary">Is it styled?</AccordionSummary>
+    <AccordionContent>
+      Yes. It comes with default styles that matches the other components'
+      aesthetic.
+    </AccordionContent>
+  </AccordionItem>
+  <AccordionItem>
+    <AccordionSummary slot="summary">Is it animated?</AccordionSummary>
+    <AccordionContent>
+      Yes! You can animate the Accordion with CSS or JavaScript.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
