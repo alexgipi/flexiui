9de53bbd55dd4854d33976f46f5fd8a2b9e6c387
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:47:48 +0200

    feat: enhance Quote component documentation with detailed descriptions and beta warning

--- docs/src/content/docs/Quote/examples/QuoteBasicExample.svelte
+++ docs/src/content/docs/Quote/examples/QuoteBasicExample.svelte
@@ -3,5 +3,7 @@
 </script>
 
 <Quote className="my-quote" id="my-quote-id" bgColor="transparent">
-  This is a quote component example with editable mode enabled.
+  This is a simple and flexible "Quote" component designed to highlight phrases or important content. 
+  It allows easy customization of styles, icons, colors, and more.Additionally, it includes an 
+  editable mode for use in forms, block editors, and visual editors.
 </Quote>
