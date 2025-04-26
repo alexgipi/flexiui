89aba7391437e24ecfe9fc48bfa3baa413968fa5
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:42:31 +0200

    feat: add AlertSeverityExample component to demonstrate various alert severities

---
+++ docs/src/content/docs/accordion/examples/AlertSeverityExample.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Alert from '@components/ui/Alert.svelte'
+</script>
+
+<Alert severity="info">
+  This is a basic alert with <strong>info</strong> severity.
+</Alert>
+<Alert severity="success">
+  This is a basic alert with <strong>success</strong> severity.
+</Alert>
+<Alert severity="warning">
+  This is a basic alert with <strong>warning</strong> severity.
+</Alert>
+<Alert severity="error">
+  This is a basic alert with <strong>error</strong> severity.
+</Alert>
+<Alert severity="tip">
+  This is a basic alert with <strong>tip</strong> severity.
+</Alert>
