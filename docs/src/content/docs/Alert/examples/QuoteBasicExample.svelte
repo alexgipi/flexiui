951c79a6fa9472b12452cc316179c35b3eb496b3
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:46:50 +0200

    refactor: remove unused Quote component examples from documentation

--- docs/src/content/docs/Alert/examples/QuoteBasicExample.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import { Quote } from "@flexiui/svelte-quote";
-</script>
-
-<Quote className="my-quote" id="my-quote-id" bgColor="transparent">
-  This is a quote component example with editable mode enabled.
-</Quote>
