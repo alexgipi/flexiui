bb64eba20c7178b9f42c0b15b055495bebe10ad8
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-04-26 05:41:53 +0200

    feat: add TabPanel component for managing active tab content visibility

---
+++ docs/src/components/ui/Tabs/TabPanel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let active: boolean = false;
+</script>
+
+<div class="tab-panel" class:active={active}>
+    <slot></slot>
+</div>
+
+<style>
+    .tab-panel {
+      display: none;
+    }
+  
+    .tab-panel.active {
+      display: block;
+    }  
+</style>
