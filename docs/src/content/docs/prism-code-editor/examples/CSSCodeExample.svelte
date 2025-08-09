586e00384f09eca07778ec6deb48c3215e06cb1c
alexgipi <53187869+alexgipi@users.noreply.github.com>
2025-08-09 04:32:25 +0200

    feat(prism-code-editor): add CSS and HTML code examples with editable mode

---
+++ docs/src/content/docs/prism-code-editor/examples/CSSCodeExample.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { PrismCodeEditor } from "@flexiui/svelte-prism-code-editor";
+
+  let value: string | null = null;
+  let editor: any;
+
+  function onUpdate(e: any) {
+    const { eventType, code } = e.detail;
+
+    if (eventType === "update") {
+      value = code;
+    }
+  }
+
+  function updateCode() {
+    editor.setOptions({ value: "Hola" });
+  }
+</script>
+
+<PrismCodeEditor
+  bind:editor
+  on:update={(e: any) => onUpdate(e)}
+  language="css"
+  id="css-example-code-editor"
+  {value}
+  className="no-drag"
+></PrismCodeEditor>
