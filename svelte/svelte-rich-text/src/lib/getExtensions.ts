4decf3c30e2ff9f4359a449d9bdfbbb60d9b9ed6
alexgipi <53187869+alexgipi@users.noreply.github.com>
2026-01-11 16:25:59 +0100

    feat(svelte/svelte-rich-text): Add getRichTextExtensions to aggregate rich text editor extensions an

---
+++ svelte/svelte-rich-text/src/lib/getExtensions.ts
@@ -0,0 +1,72 @@
+// getExtensions.ts
+import StarterKit from "@tiptap/starter-kit";
+import Highlight from "@tiptap/extension-highlight";
+import TextAlign from "@tiptap/extension-text-align";
+import Image from "@tiptap/extension-image";
+import { ListKit } from "@tiptap/extension-list";
+import { TextStyleKit } from "@tiptap/extension-text-style";
+import { Mathematics } from "@tiptap/extension-mathematics";
+import { TableKit } from "@tiptap/extension-table";
+
+import { Audio } from "./extensions/Audio";
+import { NodeLineHeight } from "./extensions/NodeLineHeight";
+import { MediaGridExtension } from "./extensions/MediaGrid/MediaGrid";
+import { MediaGridItemExtension } from "./extensions/MediaGrid/MediaGridItem";
+import { CustomTableCell } from "./extensions/Table/CustomTableCell";
+import { CustomTableHeader } from "./extensions/Table/CustomTableHeader";
+import { EnhancedLink } from "./extensions/EnhancedLink";
+
+export function getRichTextExtensions(options?: {
+  editable?: boolean;
+  customExtensions?: any[];
+}) {
+  const { editable = false, customExtensions = [] } = options ?? {};
+
+  return [
+    Highlight.configure({ multicolor: true }),
+    TextStyleKit,
+    StarterKit.configure({
+      trailingNode: false,
+      link: false,
+      bulletList: false,
+      listItem: false,
+      orderedList: false,
+      listKeymap: false,
+    }),
+    EnhancedLink,
+    Image.configure({ inline: true }),
+    Audio.configure({
+      HTMLAttributes: { class: "audio-player" },
+    }),
+    ListKit,
+    TextAlign.configure({
+      types: [
+        "heading",
+        "paragraph",
+        "bulletList",
+        "taskList",
+        "listItem",
+        "blockquote",
+      ],
+    }),
+    NodeLineHeight,
+    MediaGridExtension,
+    MediaGridItemExtension,
+    TableKit.configure({
+      table: {
+        HTMLAttributes: { class: "fl-table-editable" },
+        resizable: true,
+      },
+      tableCell: false,
+      tableHeader: false,
+    }),
+    CustomTableCell.configure({
+      HTMLAttributes: { class: "fl-cell-editable" },
+    }),
+    CustomTableHeader.configure({
+      HTMLAttributes: { class: "fl-cell-editable" },
+    }),
+    !editable ? Mathematics : null,
+    ...customExtensions,
+  ];
+}
