5c8f5c97c09c67cceb7c044e567af0393bc67785
alexgipi <53187869+alexgipi@users.noreply.github.com>
2026-01-15 23:36:40 +0100

    feat(svelte/svelte-rich-text): feat: export Placeholder and Audio extensions for Rich Text

--- svelte/svelte-rich-text/src/lib/index.ts
+++ svelte/svelte-rich-text/src/lib/index.ts
@@ -1,4 +1,11 @@
 import RichText from './RichText.svelte';
 import { renderHTMLFromJSON } from './renderRichText';
+import { Placeholder as PlaceholderExt } from './extensions/Placeholder';
+import { Audio as AudioExt } from './extensions/Audio';
 
-export { RichText, renderHTMLFromJSON };
+export {
+    RichText,
+    PlaceholderExt,
+    AudioExt,
+    renderHTMLFromJSON
+};
