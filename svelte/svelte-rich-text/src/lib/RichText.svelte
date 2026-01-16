acfb3589dba088658ff05f4c3b4c17e5e5989860
alexgipi <53187869+alexgipi@users.noreply.github.com>
2026-01-16 01:34:08 +0100

    fix(svelte/svelte-rich-text): fix: use currentColor for Audio icon to match theme colors.

--- svelte/svelte-rich-text/src/lib/RichText.svelte
+++ svelte/svelte-rich-text/src/lib/RichText.svelte
@@ -1292,7 +1292,7 @@
           aria-label="Audio"
           class:is-active={$editor.isActive("audio")}
         >
-          <svg style="transform: scale(1.1);" fill="#e8eaed" width="24px" viewBox="0 -960 960 960" height="24px" xmlns="http://www.w3.org/2000/svg"><path d="M400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-422h240v160H560v400q0 66-47 113t-113 47Z"></path></svg>
+          <svg style="transform: scale(1.1);" fill="currentColor" width="24px" viewBox="0 -960 960 960" height="24px" xmlns="http://www.w3.org/2000/svg"><path d="M400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-422h240v160H560v400q0 66-47 113t-113 47Z"></path></svg>
         </button>
       </div>
 
