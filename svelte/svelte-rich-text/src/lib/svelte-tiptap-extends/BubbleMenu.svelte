bae34f728d9b1adcabb462ddea97cd40b2f86f96
alexgipi <53187869+alexgipi@users.noreply.github.com>
2026-03-19 14:32:50 +0100

    feat(rich-text): add custom BubbleMenu component

---
+++ svelte/svelte-rich-text/src/lib/svelte-tiptap-extends/BubbleMenu.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { BubbleMenuPlugin, type BubbleMenuPluginProps } from '@tiptap/extension-bubble-menu';
+
+  import type { ComponentInputProps } from './types';
+  import { invariant } from './utils';
+
+  type Props = ComponentInputProps<BubbleMenuPluginProps>;
+
+  const {
+    editor,
+    options = {},
+    pluginKey = 'SvelteTiptapBubbleMenu',
+    shouldShow = null,
+    updateDelay = 250,
+    appendTo = null,
+    children,
+    class: className,
+  }: Props = $props();
+
+  invariant(editor, 'Missing editor instance.');
+
+  let element: HTMLElement;
+
+  onMount(() => {
+    element.style.visibility = 'hidden';
+    element.style.position = 'absolute';
+
+    const plugin = BubbleMenuPlugin({
+      pluginKey,
+      editor,
+      element,
+      options,
+      shouldShow,
+      updateDelay,
+      appendTo,
+    });
+
+    editor.registerPlugin(plugin);
+
+    return () => {
+      editor?.unregisterPlugin(pluginKey);
+      window.requestAnimationFrame(() => {
+        if (element && element.parentNode) {
+          element.parentNode.removeChild(element);
+        }
+      });
+    };
+  });
+</script>
+
+<div bind:this={element} class={className} style="visibility: hidden;">
+  {#if children}
+    {@render children()}
+  {/if}
+</div>
